848b669b8c7c44bb466dd68e684fae90a863e0c5
Skeltch <TheSkeltch@gmail.com>
2016-04-03 04:07:34 -0400

    Remove duplicated directory

--- src/main/java/group14/tutoru/AsyncResponse.java
+++
@@ -1,9 +0,0 @@
-package group14.tutoru;
-
-/**
- * Created by Sam on 3/15/2016.
- */
-//Interface for when the asynctask has finished
-public interface AsyncResponse {
-    void processFinish(String output);
-}
